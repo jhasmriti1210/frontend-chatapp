59ccc1ab508492bb6ed0d57816579d75cde4ecc0
jhasmriti1210 <156930687+jhasmriti1210@users.noreply.github.com>
2024-10-03 23:23:59 +0530

    hi

--- constants/config.js
+++ constants/config.js
@@ -1,9 +1,7 @@
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "http://localhost:4173",
-    process.env.CLIENT_URL,
-    "https://frontend-chatapp-liard.vercel.app",
+
+    'https://frontend-chatapp-eta.vercel.app',
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
