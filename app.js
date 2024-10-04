96683efb85ba55d683208676b7f0affec6f95738
jhasmriti1210 <156930687+jhasmriti1210@users.noreply.github.com>
2024-10-04 21:00:27 +0530

    hii

--- app.js
+++ app.js
@@ -56,7 +56,12 @@ app.set("io", io);
 // Using Middlewares Here
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://frontend-chatapp-eta.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next(); // Passes the request to the next middleware or route handler
+});
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/chat", chatRoute);
